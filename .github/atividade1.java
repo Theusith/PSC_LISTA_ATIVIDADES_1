beb6781a767efdc9a62c8d205c5e9d2f50cd1ac2
Matheus <129126932+Theusith@users.noreply.github.com>
2024-03-04 22:22:24 -0300

    atividade1.java

---
+++ .github/atividade1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+Atividade 1:
+    public static void main(String[] args) {
+            int num1, num2, soma;
+	    Scanner sc = new Scanner(System.in);
+	    System.out.println("Digite o primeiro numero:");
+	    num1 = sc.nextInt();
+	    System.out.println("Escreva o segundo numero:");
+	    num2 = sc.nextInt();
+	    soma = num1+num2;
+	    System.out.println("A soma dos numeros é:" + soma);
+}
+}
