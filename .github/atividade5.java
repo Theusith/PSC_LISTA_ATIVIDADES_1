577976097d0037e65300ac609240c09a6ca8cb38
Matheus Oliveira <129126932+Theusith@users.noreply.github.com>
2024-03-10 11:42:43 -0300

    Update atividade5.java

--- .github/atividade5.java
+++ .github/atividade5.java
@@ -1,3 +1,5 @@
+/*Crie um algoritmo que lê um valor real em dólar, e converte o valor para reais. Considere que a cotação é US$ 1 = R$ 4,95*/
+
 import java.util.Scanner;
 
 public class Atividade5 {
