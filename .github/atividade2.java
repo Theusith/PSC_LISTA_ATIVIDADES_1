9c09c6ba35b090269b362ae03b4733a5e419433c
Matheus Oliveira <129126932+Theusith@users.noreply.github.com>
2024-03-10 10:00:10 -0300

    Create atividade2.java

---
+++ .github/atividade2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Atividade2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num1, num2;
+		float resto;
+		
+		System.out.println("Escreva o primeiro numero:");
+		num1 = sc.nextInt();
+		System.out.println("Escreva o segundo numero:");
+		num2 = sc.nextInt();
+		resto = num1 % num2;
+		System.out.println("o Resto da davisão dos dois numeros é:" + resto);
+		sc.close();
+
+	}
+
+}
